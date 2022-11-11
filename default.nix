bec2412d776dd43033ba13c77f9bf1f187b6127c
dimstav23 <dimstav23@gmail.com>
2022-11-11 21:50:13 +0000

    add default.nix

---
+++ default.nix
@@ -0,0 +1,19 @@
+with import <nixpkgs> { };
+mkShell {
+  nativeBuildInputs = [
+    bashInteractive
+    # to run tests
+    dnsmasq
+    pkg-config
+    qemu
+    libvirt
+    virt-manager
+    vim
+    libuuid
+    nasm
+    file
+    bridge-utils
+    cloud-utils
+    ];
+}
+
