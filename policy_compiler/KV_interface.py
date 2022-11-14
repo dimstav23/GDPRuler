d528dc928beb02b803c47a279ee4852b1c9044c8
dimstav23 <dimstav23@gmail.com>
2022-11-14 22:56:23 +0000

    potential KV interface skeleton

---
+++ policy_compiler/KV_interface.py
@@ -0,0 +1,29 @@
+# intact KV API
+def put(K, V):
+    return
+
+def get(K, V):
+    return
+
+def delete(K, V):
+    return
+
+# GDPR-enhanced API
+def put_filtered(K, V, filter):
+    return
+
+def get_filtered(K, V, filter):
+    return
+
+def delete_filtered(K, V, filter):
+    return
+
+# GDPR-metadata API
+def putm():
+    return
+
+def getm():
+    return
+
+def deletem():
+    return
