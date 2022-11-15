c58f5b0beda3cac34ac2d8755d6ada903d14dd66
dimstav23 <dimstav23@gmail.com>
2022-11-15 23:29:39 +0000

    add rocksdb submodule

--- default.nix
+++ default.nix
@@ -2,7 +2,6 @@ with import <nixpkgs> { };
 mkShell {
   nativeBuildInputs = [
     bashInteractive
-    # to run tests
     dnsmasq
     pkg-config
     qemu
@@ -15,11 +14,20 @@ mkShell {
     bridge-utils
     cloud-utils
     python3
+
     #redis specific packages
     tcl
     tcltls
     openssl
     jemalloc
+
+    #rockdb specific packages
+    clang-tools
+    lz4
+    bzip2
+    snappy
+    zlib
+    gflags
     ];
 }
 
