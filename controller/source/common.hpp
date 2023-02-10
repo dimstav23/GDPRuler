991dc432a1d6afa7598e24ec1b8933e8643edb4a
dimstav23 <dimstav23@gmail.com>
2023-02-10 09:26:31 +0000

    commit missing files for compilation in the VM and include additional std headers

---
+++ controller/source/common.hpp
@@ -0,0 +1,4 @@
+#pragma once
+
+constexpr int s2ns = 1000000000;
+constexpr int ns_precision = 9;
--- controller/source/gdpr_metadata.hpp
+++ controller/source/gdpr_metadata.hpp
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <algorithm>
+#include <vector>
 #include <unordered_map>
 
 namespace controller {
