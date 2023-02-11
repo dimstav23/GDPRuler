bcc6c5a130fa615cbe2d1de2e0b274518a82b451
dimstav23 <dimstav23@gmail.com>
2023-02-11 16:51:16 +0000

    script for native executions of redis for GDPR and vanilla versions -- 5 repeats for each workload

--- benchmarks/redis_runner.sh
+++ benchmarks/redis_runner.sh
@@ -7,25 +7,36 @@ workloads="workloada workloadb workloadc workloadd workloadf"
 redis_server_path=../KVs/redis/src
 
 mkdir -p results
+rm -rf ./dump.rdb
+rm -rf ${redis_server_path}/dump.rdb
 
 # Vanilla native version
 for workload in $workloads; do
   controller_times=0
   system_times=0
   for rep in $(seq 1 $repeats); do
-    ${redis_server_path}/redis_server &
+    echo "Running Vanilla version with $workload... (repeat: $rep)"
+    ${redis_server_path}/redis-server &> /dev/null &
     redis_pid=$!
+    sleep 3
     python3 ../native_ctl.py -w ${workload_folder}/${workload} > ./results/native_${workload}_${rep}
     controller_time=$(grep "Controller time:" ./results/native_${workload}_${rep} | awk '{print $3}')
     system_time=$(grep "System time:" ./results/native_${workload}_${rep} | awk '{print $3}')
-    controller_times=$(echo "$controller_times + $controller_time" | bc)
-    system_times=$(echo "$system_times + $system_time" | bc)
+    controller_times=$(echo "$controller_times + $controller_time" | bc -l)
+    system_times=$(echo "$system_times + $system_time" | bc -l)
     kill $redis_pid
+    sleep 3
     rm -rf ./dump.rdb
     rm -rf ${redis_server_path}/dump.rdb
   done
-  avg_controller_time=$(echo "$controller_times / $repeats" | bc)
-  avg_system_time=$(echo "$system_times / $repeats" | bc)
+  avg_controller_time=$(echo "$controller_times / $repeats" | bc -l | awk '{printf "%.9f", $0}')
+  avg_system_time=$(echo "$system_times / $repeats" | bc -l | awk '{printf "%.9f", $0}')
+  if [ $(echo "$avg_controller_time < 0" | bc) -eq 1 ]; then
+    avg_controller_time="0$avg_controller_time"
+  fi
+  if [ $(echo "$avg_system_time < 0" | bc) -eq 1 ]; then
+    avg_system_time="0$avg_system_time"
+  fi
   echo "$workload,controller_time,$avg_controller_time" >> ./results/native_average.csv
   echo "$workload,system_time,$avg_system_time" >> ./results/native_average.csv
 done
@@ -35,41 +46,50 @@ for workload in $workloads; do
   controller_times=0
   system_times=0
   for rep in $(seq 1 $repeats); do
-    ${redis_server_path}/redis_server &
+    echo "Running GDPR version with $workload... (repeat: $rep)"
+    ${redis_server_path}/redis-server &> /dev/null &
     redis_pid=$!
+    sleep 3
     python3 ../GDPRuler.py -c ${default_policy_cfg} -w ${workload_folder}/${workload} > ./results/gdpr_${workload}_${rep}
     controller_time=$(grep "Controller time:" ./results/gdpr_${workload}_${rep} | awk '{print $3}')
     system_time=$(grep "System time:" ./results/gdpr_${workload}_${rep} | awk '{print $3}')
-    controller_times=$(echo "$controller_times + $controller_time" | bc)
-    system_times=$(echo "$system_times + $system_time" | bc)
+    controller_times=$(echo "$controller_times + $controller_time" | bc -l)
+    system_times=$(echo "$system_times + $system_time" | bc -l)
     kill $redis_pid
+    sleep 3
     rm -rf ./dump.rdb
     rm -rf ${redis_server_path}/dump.rdb
-  done
-  avg_controller_time=$(echo "$controller_times / $repeats" | bc)
-  avg_system_time=$(echo "$system_times / $repeats" | bc)
+  done 
+  avg_controller_time=$(echo "$controller_times / $repeats" | bc -l | awk '{printf "%.9f", $0}')
+  avg_system_time=$(echo "$system_times / $repeats" | bc -l | awk '{printf "%.9f", $0}')
+  if [ $(echo "$avg_controller_time < 0" | bc) -eq 1 ]; then
+    avg_controller_time="0$avg_controller_time"
+  fi
+  if [ $(echo "$avg_system_time < 0" | bc) -eq 1 ]; then
+    avg_system_time="0$avg_system_time"
+  fi
   echo "$workload,controller_time,$avg_controller_time" >> ./results/gdpr_average.csv
   echo "$workload,system_time,$avg_system_time" >> ./results/gdpr_average.csv
 done
 
-# SEV GDPRuler version
-for workload in $workloads; do
-  controller_times=0
-  system_times=0
-  for rep in $(seq 1 $repeats); do
-    ${redis_server_path}/redis_server &
-    redis_pid=$!
-    python3 ../GDPRuler.py -c ${default_policy_cfg} -w ${workload_folder}/${workload} > ./results/sec_gdpr_${workload}_${rep}
-    controller_time=$(grep "Controller time:" ./results/sec_gdpr_${workload}_${rep} | awk '{print $3}')
-    system_time=$(grep "System time:" ./results/sec_gdpr_${workload}_${rep} | awk '{print $3}')
-    controller_times=$(echo "$controller_times + $controller_time" | bc)
-    system_times=$(echo "$system_times + $system_time" | bc)
-    kill $redis_pid
-    rm -rf ./dump.rdb
-    rm -rf ${redis_server_path}/dump.rdb
-  done
-  avg_controller_time=$(echo "$controller_times / $repeats" | bc)
-  avg_system_time=$(echo "$system_times / $repeats" | bc)
-  echo "$workload,controller_time,$avg_controller_time" >> ./results/sec_gdpr_average.csv
-  echo "$workload,system_time,$avg_system_time" >> ./results/sec_gdpr_average.csv
-done
+# # SEV GDPRuler version
+# for workload in $workloads; do
+#   controller_times=0
+#   system_times=0
+#   for rep in $(seq 1 $repeats); do
+#     ${redis_server_path}/redis_server &
+#     redis_pid=$!
+#     python3 ../GDPRuler.py -c ${default_policy_cfg} -w ${workload_folder}/${workload} > ./results/sec_gdpr_${workload}_${rep}
+#     controller_time=$(grep "Controller time:" ./results/sec_gdpr_${workload}_${rep} | awk '{print $3}')
+#     system_time=$(grep "System time:" ./results/sec_gdpr_${workload}_${rep} | awk '{print $3}')
+#     controller_times=$(echo "$controller_times + $controller_time" | bc)
+#     system_times=$(echo "$system_times + $system_time" | bc)
+#     kill $redis_pid
+#     rm -rf ./dump.rdb
+#     rm -rf ${redis_server_path}/dump.rdb
+#   done
+#   avg_controller_time=$(echo "$controller_times / $repeats" | bc)
+#   avg_system_time=$(echo "$system_times / $repeats" | bc)
+#   echo "$workload,controller_time,$avg_controller_time" >> ./results/sec_gdpr_average.csv
+#   echo "$workload,system_time,$avg_system_time" >> ./results/sec_gdpr_average.csv
+# done
