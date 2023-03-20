3c5ddd1e157663045af5c1024c2e31efc7db36a3
dimstav23 <dimstav23@gmail.com>
2023-03-20 13:20:49 +0000

    isolate the controller and system time from test execution & store to a .csv file only the avg time per client for each experiment --- detailed information for each client is still printed to stdout

--- test_workloads.sh
+++ test_workloads.sh
@@ -21,6 +21,9 @@ run_test() {
     sleep 3
   fi
 
+  local controller_times=0
+  local system_times=0
+
   # start clients in parallel and redirect their outputs to different files
   for ((i=1; i<=$n_clients; i++)); do
     python GDPRuler.py --config ./configs/owner_policy.json --workload ./workload_traces/${workload_name} --db ${db} > ${test_outputs_folder}/${test_name_suffix}_client_${i}.txt &
@@ -38,15 +41,39 @@ run_test() {
       exit 1
     else
       echo "Client $i in test $test_name_suffix finished successfully. Output:"
+      # Direct client output to stdout
       cat ${test_outputs_folder}/${test_name_suffix}_client_${i}.txt
+      # Retrieve the client results from the temp files
+      controller_time=$(grep "Controller time:" ${test_outputs_folder}/${test_name_suffix}_client_${i}.txt | awk '{print $3}')
+      system_time=$(grep "System time:" ${test_outputs_folder}/${test_name_suffix}_client_${i}.txt | awk '{print $3}')
+      controller_times=$(echo "$controller_times + $controller_time" | bc -l)
+      system_times=$(echo "$system_times + $system_time" | bc -l)
+      # Remove the temp file for client output
+      rm ${test_outputs_folder}/${test_name_suffix}_client_${i}.txt
     fi
   done
 
   if [ $db == rocksdb ]; then
     # Stop rocksdb server
     kill $(pgrep rocksdb_server)
+    # Remove the temp file for server output
+    rm ${test_outputs_folder}/${test_name_suffix}_server.txt
     sleep 3
   fi
+
+  # Calculate the average controller time & system time per client
+  avg_controller_time=$(echo "$controller_times / $n_clients" | bc -l | awk '{printf "%.9f", $0}')
+  avg_system_time=$(echo "$system_times / $n_clients" | bc -l | awk '{printf "%.9f", $0}')
+  if [ $(echo "$avg_controller_time < 0" | bc) -eq 1 ]; then
+    avg_controller_time="0$avg_controller_time"
+  fi
+  if [ $(echo "$avg_system_time < 0" | bc) -eq 1 ]; then
+    avg_system_time="0$avg_system_time"
+  fi
+  # Write the avg controller and system time to the result file
+  echo "$workload,controller_time,$avg_controller_time" > ${test_outputs_folder}/${test_name_suffix}.csv
+  echo "$workload,system_time,$avg_system_time" > ${test_outputs_folder}/${test_name_suffix}.csv
+
 }
 
 # build controller
@@ -61,10 +88,14 @@ mkdir -p ${test_outputs_folder}
 # TESTS with combibations of
 #   {1,2,4,8,16,32} clients,
 #   {redis, rocksdb} dbs,
-#   {workloada_test, workloadb_test} workloads
-for ((n_clients=1; n_clients<=32; n_clients=n_clients*2)); do
-  for db in rocksdb redis; do
-    for workload in workloada_test workloadb_test; do
+#   {workloada_test, workloadb_test, workloadc_test, workloadd_test, workloadf_test} workloads
+clients="1 2 4 8 16 32"
+dbs="rocksdb"
+workloads="workloada_test workloadb_test workloadc_test workloadd_test workloadf_test"
+
+for n_clients in $clients; do
+  for db in $dbs; do
+    for workload in $workloads; do
       echo "Starting a test with $n_clients clients, $db store, and $workload."
       run_test $n_clients $workload $db
       echo ""
