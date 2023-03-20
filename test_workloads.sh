3cd77c17c4a47cfa1a4a2daecc71e5edf6f2e5ba
Ertugrul Aypek <ertugrulaypek@gmail.com>
2023-03-20 02:15:02 +0000

    update test script to adapt latest server

--- test_workloads.sh
+++ test_workloads.sh
@@ -4,20 +4,18 @@ test_outputs_folder="test_outputs"
 
 # start a test by running the server and clients
 # Args:
-#   1: n_server_threads   (number of threads to run the server on)
-#   2: n_clients          (number of clients to run concurrently)
-#   3: workload_path      (workload file name expected to be in ./workload_traces/ dir)
-#   4: db                 (db to be used in controller. one of {rocksdb, redis})
+#   1: n_clients          (number of clients to run concurrently)
+#   2: workload_path      (workload file name expected to be in ./workload_traces/ dir)
+#   3: db                 (db to be used in controller. one of {rocksdb, redis})
 run_test() {
-  local n_server_threads=$1
-  local n_clients=$2
-  local workload_name=$3
-  local db=$4
-  local test_name_suffix=${workload_name}_${db}_${n_server_threads}_${n_clients}
+  local n_clients=$1
+  local workload_name=$2
+  local db=$3
+  local test_name_suffix=${workload_name}_${db}_${n_clients}
 
   if [ $db == rocksdb ]; then
     # run rocksdb server
-    ./controller/build/rocksdb_server 15001 ./db $n_server_threads > ${test_outputs_folder}/${test_name_suffix}_server.txt &
+    ./controller/build/rocksdb_server 15001 ./db > ${test_outputs_folder}/${test_name_suffix}_server.txt &
 
     # wait for server to start up
     sleep 3
@@ -61,18 +59,15 @@ cd ..
 mkdir -p ${test_outputs_folder}
 
 # TESTS with combibations of
-#   {1,2,4,8,16,32} threads,
 #   {1,2,4,8,16,32} clients,
 #   {redis, rocksdb} dbs,
 #   {workloada_test, workloadb_test} workloads
-for ((n_server_threads=1; n_server_threads<=32; n_server_threads=n_server_threads*2)); do
-  for ((n_clients=1; n_clients<=32; n_clients=n_clients*2)); do
-    for db in rocksdb redis; do
-      for workload in workloada_test workloadb_test; do
-        echo "Starting a test with $n_server_threads threads, $n_clients clients, $db store, and $workload."
-        run_test $n_server_threads $n_clients $workload $db
-        echo ""
-      done
+for ((n_clients=1; n_clients<=32; n_clients=n_clients*2)); do
+  for db in rocksdb redis; do
+    for workload in workloada_test workloadb_test; do
+      echo "Starting a test with $n_clients clients, $db store, and $workload."
+      run_test $n_clients $workload $db
+      echo ""
     done
   done
 done
