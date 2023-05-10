aeaf3f036c0d3951e5174577214f47b9546bd423
dimstav23 <dimstav23@gmail.com>
2023-05-10 21:31:18 +0000

    adapted encrypt/decrypt functions for aesgcm128, usage of IV and inclusion of MAC

--- controller/source/encryption/cipher_engine.hpp
+++ controller/source/encryption/cipher_engine.hpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <optional>
 #include <string>
+#include <vector>
 
 #include <openssl/aes.h>
 #include <openssl/evp.h>
@@ -16,13 +17,14 @@
 namespace controller
 {
 
-constexpr int encryption_key_len = 32;
-constexpr int initialization_vector_len = 16;
+constexpr int encryption_key_len = 16; // 128 bits for AES-GCM-128
+constexpr int initialization_vector_len = 12; // 96 bits for AES-GCM
+constexpr int tag_len = 16; // 128 bits for AES-GCM-128
 
-class encrpyt_result
+class encrypt_result
 {
 public:
-  encrpyt_result(std::string ciphertext, bool success)
+  encrypt_result(std::string ciphertext, bool success)
       : m_ciphertext {std::move(ciphertext)}
       , m_success {success}
   {
@@ -32,10 +34,10 @@ public:
   bool m_success;
 };
 
-class decrpyt_result
+class decrypt_result
 {
 public:
-  decrpyt_result(std::string plaintext, bool success)
+  decrypt_result(std::string plaintext, bool success)
       : m_plaintext {std::move(plaintext)}
       , m_success {success}
   {
@@ -64,121 +66,147 @@ public:
    * If successful, output's first initialization_vector_len chars contains initialization vector (iv) in plain text.
    * The rest of the output string contains ciphered form of the input based on encryption key and randomly generated iv.
   */
-  auto encrypt(const std::string& input) -> encrpyt_result {
-    static encrpyt_result failed_encrpyt_result {{}, /*success*/ false};
+  auto encrypt(const std::string& input) -> encrypt_result {
+    static encrypt_result failed_encrypt_result {{}, /*success*/ false};
+    
+    /* Generate a random IV */
     unsigned char initialization_vector[initialization_vector_len];
-    RAND_bytes(initialization_vector, initialization_vector_len);
-
-    EVP_CIPHER_CTX* ctx = nullptr;
-
-    /* +1 to include the null termination */
-    int input_len = static_cast<int>(input.size()) + 1;
-
-    /* max ciphertext len for a n bytes of plaintext is n + AES_BLOCK_SIZE -1 bytes */
-    auto ciphertext = new unsigned char[static_cast<size_t>(input_len + AES_BLOCK_SIZE)];
-    int temp_len, ciphertext_len;
-    auto input_ptr = reinterpret_cast<const unsigned char*>(input.c_str());
+    if (RAND_bytes(initialization_vector, sizeof(initialization_vector)) != 1) {
+      std::cerr << "Failed to generate random initialization vector!" << std::endl;
+      return failed_encrypt_result;
+    }
 
     /* Create and initialise the context */
-    if (!(ctx = EVP_CIPHER_CTX_new())) {
-      std::cout << "Could not initialize encrypt context!" << std::endl;
-      return failed_encrpyt_result;
+    EVP_CIPHER_CTX* ctx = EVP_CIPHER_CTX_new();
+    if (ctx == nullptr) {
+      std::cerr << "Failed to create encryption context!" << std::endl;
+      return failed_encrypt_result;
     }
 
     /* Initialise the encryption operation. */
-    if (1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_gcm(), nullptr, m_key, initialization_vector)) {
-      std::cout << "Could not initialize encrypt operation!" << std::endl;
-      return failed_encrpyt_result;
+    if (EVP_EncryptInit_ex(ctx, EVP_aes_128_gcm(), nullptr, m_key, initialization_vector) != 1) {
+      std::cerr << "Failed to initialize encryption!" << std::endl;
+      EVP_CIPHER_CTX_free(ctx);
+      return failed_encrypt_result;
     }
 
-    /* Provide the plaintext to be encrypted, and obtain the encrypted output.
-     */
-    if (1 != EVP_EncryptUpdate(ctx, ciphertext, &temp_len, input_ptr, input_len)) {
-      std::cout << "Could not update encrypt!" << std::endl;
-      return failed_encrpyt_result;
+    int ciphertext_len = 0;
+    /* max ciphertext len for a n bytes of plaintext is n + AES_BLOCK_SIZE bytes */
+    unsigned char ciphertext[input.size() + EVP_CIPHER_CTX_block_size(ctx)];
+    /* Provide the plaintext to be encrypted, and obtain the encrypted output. */
+    if (EVP_EncryptUpdate(ctx, ciphertext, &ciphertext_len, reinterpret_cast<const unsigned char*>(input.data()), static_cast<int>(input.size())) != 1) {
+      std::cerr << "Failed to perform encryption!" << std::endl;
+      EVP_CIPHER_CTX_free(ctx);
+      return failed_encrypt_result;
     }
-    ciphertext_len = temp_len;
 
     /* Finalise the encryption. */
-    if (1 != EVP_EncryptFinal_ex(ctx, ciphertext + temp_len, &temp_len)) {
-      std::cout << "Could not finalize encrypt!" << std::endl;
-      return failed_encrpyt_result;
+    int final_len = 0;
+    if (EVP_EncryptFinal_ex(ctx, ciphertext + ciphertext_len, &final_len) != 1) {
+      std::cerr << "Failed to finalize encryption!" << std::endl;
+      EVP_CIPHER_CTX_free(ctx);
+      return failed_encrypt_result;
     }
-    ciphertext_len += temp_len;
+
+    ciphertext_len += final_len;
+
+    /* Retrieve the MAC (Message Authentication Code) of the ciphertext */
+    std::vector<unsigned char> mac(tag_len);
+    if (EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_AEAD_GET_TAG, tag_len, mac.data()) != 1) {
+      std::cerr << "Failed to extract MAC!" << std::endl;
+      EVP_CIPHER_CTX_free(ctx);
+      return failed_encrypt_result;
+    }
 
     /* Clean up */
     EVP_CIPHER_CTX_free(ctx);
 
-    /* Return successful encrypt result. Exclude null termination */
-    std::string result_string{};
-    result_string.append(reinterpret_cast<char*>(initialization_vector), initialization_vector_len);
-    result_string.append(reinterpret_cast<char*>(ciphertext), static_cast<size_t>(ciphertext_len - 1));
-    delete ciphertext;
-    return encrpyt_result {result_string,true};
+    std::string result_string;
+    result_string.append(reinterpret_cast<const char*>(initialization_vector), initialization_vector_len);
+    result_string.append(reinterpret_cast<const char*>(mac.data()), tag_len);
+
+    // Encode the ciphertext length as a 4-byte integer
+    unsigned char len_bytes[4];
+    len_bytes[0] = (ciphertext_len >> 24) & 0xFF;
+    len_bytes[1] = (ciphertext_len >> 16) & 0xFF;
+    len_bytes[2] = (ciphertext_len >> 8) & 0xFF;
+    len_bytes[3] = ciphertext_len & 0xFF;
+    result_string.append(reinterpret_cast<const char*>(len_bytes), 4);
+
+    result_string.append(reinterpret_cast<const char*>(ciphertext), ciphertext_len);
+
+    return encrypt_result {result_string, true};
   }
 
   /**
    * Decrypt given input. 
    * 
-   * First initialization_vector_len chars are expected to be initialization vector in plain text.
-   * The rest of the characters are expected to be the cipher text
-  */
-  auto decrypt(const std::string& iv_and_ciphertext) -> decrpyt_result {
-    static decrpyt_result failed_decrpyt_result {{}, false};
-    unsigned char initialization_vector[initialization_vector_len];
-    memcpy(initialization_vector, iv_and_ciphertext.c_str(), initialization_vector_len);
-    std::string ciphertext = iv_and_ciphertext.substr(initialization_vector_len);
+   * First initialization_vector_len chars are expected to be the initialization vector,
+   * the calculated MAC, the size of the encrypted value, and the actual encrypted value.
+   */
+  auto decrypt(const std::string& ciphertext) -> decrypt_result {
+    static decrypt_result failed_decrypt_result {{}, false};
 
-    /* +1 to include the null termination */
-    int ciphertext_len = static_cast<int>(ciphertext.size()) + 1;
+    // Extract the components from the result string
+    const unsigned char* iv = reinterpret_cast<const unsigned char*>(ciphertext.data());
+    const unsigned char* mac = iv + initialization_vector_len;
+    const unsigned char* len_bytes = mac + tag_len;
+    const unsigned char* encrypted_value = len_bytes + 4;
 
-    /* plaintext will always be equal to or lesser than the length of ciphertext
-     */
-    auto plaintext = new unsigned char[static_cast<size_t>(ciphertext_len)];
+    // Retrieve the ciphertext length from the 4-byte integer
+    int ciphertext_len = (len_bytes[0] << 24) | (len_bytes[1] << 16) | (len_bytes[2] << 8) | len_bytes[3];
 
-    auto ciphertext_ptr = reinterpret_cast<const unsigned char*>(ciphertext.c_str());
-
-    EVP_CIPHER_CTX* ctx;
-    int temp_len, plaintext_len;
-
-    /* Create and initialise the context */
-    if (!(ctx = EVP_CIPHER_CTX_new())) {
-      std::cout << "Could not initialize decrypt context!" << std::endl;
-      return failed_decrpyt_result;
+    // Create and initialize the context
+    EVP_CIPHER_CTX* ctx = EVP_CIPHER_CTX_new();
+    if (ctx == nullptr) {
+        std::cerr << "Failed to create decryption context!" << std::endl;
+        return failed_decrypt_result;
     }
 
-    /* Initialise the decryption operation. */
-    if (!EVP_DecryptInit_ex(ctx, EVP_aes_256_gcm(), nullptr, m_key, initialization_vector)) {
-      std::cout << "Could not initialize decrypt operation!" << std::endl;
-      return failed_decrpyt_result;
+    // Initialize the decryption operation
+    if (EVP_DecryptInit_ex(ctx, EVP_aes_128_gcm(), nullptr, m_key, iv) != 1) {
+        std::cerr << "Failed to initialize decryption!" << std::endl;
+        EVP_CIPHER_CTX_free(ctx);
+        return failed_decrypt_result;
     }
 
-    /* Provide the ciphertext to be decrypted, and obtain the plaintext. */
-    if (!EVP_DecryptUpdate(ctx, plaintext, &temp_len, ciphertext_ptr, ciphertext_len)) {
-      std::cout << "Could not update decrypt!" << std::endl;
-      return failed_decrpyt_result;
+    // Provide the ciphertext to be decrypted, and obtain the decrypted output
+    int plaintext_len = 0;
+    unsigned char plaintext[ciphertext_len];
+    if (EVP_DecryptUpdate(ctx, plaintext, &plaintext_len, encrypted_value, ciphertext_len) != 1) {
+        std::cerr << "Failed to perform decryption!" << std::endl;
+        EVP_CIPHER_CTX_free(ctx);
+        return failed_decrypt_result;
     }
-    plaintext_len = temp_len;
 
-    /* Finalise the decryption. The plaintext is decrypted and the length of the
-     * plaintext is returned. */
-    EVP_DecryptFinal_ex(ctx, plaintext + temp_len, &temp_len);
+    // Set the expected MAC value
+    if (EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_AEAD_SET_TAG, tag_len, const_cast<unsigned char*>(mac)) != 1) {
+        std::cerr << "Failed to set MAC value!" << std::endl;
+        EVP_CIPHER_CTX_free(ctx);
+        return failed_decrypt_result;
+    }
 
-    /* Clean up */
+    // Finalize the decryption
+    int final_len = 0;
+    if (EVP_DecryptFinal_ex(ctx, plaintext + plaintext_len, &final_len) != 1) {
+        std::cerr << "Failed to finalize decryption!" << std::endl;
+        EVP_CIPHER_CTX_free(ctx);
+        return failed_decrypt_result;
+    }
+    plaintext_len += final_len;
+
+    // Clean up
     EVP_CIPHER_CTX_free(ctx);
 
-    plaintext_len += temp_len;
+    auto result_string = std::string(reinterpret_cast<const char*>(plaintext), plaintext_len);
 
-    /* Return successful decrypt result. Provide (plaintext_len - 1) as length to exclude null termination */
-    auto plaintext_str = std::string(reinterpret_cast<char*>(plaintext), static_cast<size_t>(plaintext_len - 1));
-    delete plaintext;
-    return decrpyt_result { plaintext_str, true };
+    return decrypt_result { result_string, true };
   }
 
   auto init_encryption_key(const std::optional<std::string>& encryption_key = std::nullopt) -> void {
     if (encryption_key.has_value()) {
       if (encryption_key.value().size() != encryption_key_len) {
-        std::cout << "Failed to set encryption key. Expected length is " << encryption_key_len 
+        std::cerr << "Failed to set encryption key. Expected length is " << encryption_key_len 
                   << ", given length is " <<  encryption_key.value().size()
                   << ". Falling back to the default key." << std::endl;
         return;
@@ -189,8 +217,8 @@ public:
 
   private:
     cipher_engine() = default;
-
-    unsigned char m_key[encryption_key_len] = "0123456789012345678901234567890";
+    // default encryption key
+    unsigned char m_key[encryption_key_len] = "012345678901234";
   // NOLINTEND
 };
 
