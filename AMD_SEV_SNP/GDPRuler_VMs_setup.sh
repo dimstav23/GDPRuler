3a3159cf5d8e17f805b078ab8b54a982fdc78848
dimstav23 <dimstav23@gmail.com>
2024-03-07 17:28:52 +0000

    [AMD_SEV_SNP] add the controller VM setup in the cloud-config, network-config and GDPRuler_VMs_setup.sh script
    Signed-off-by: dimstav23 <dimstav23@gmail.com>

--- AMD_SEV_SNP/GDPRuler_VMs_setup.sh
+++ AMD_SEV_SNP/GDPRuler_VMs_setup.sh
@@ -47,25 +47,34 @@ fi
 IMG_URL=https://cloud-images.ubuntu.com/jammy/current
 CLOUD_IMG=jammy-server-cloudimg-amd64.img
 if [[ ! -f ${CLOUD_IMG} ]] ; then
-    wget ${IMG_URL}/${CLOUD_IMG}
+  wget ${IMG_URL}/${CLOUD_IMG}
 fi
 
 # Create the appropriate directories
 mkdir -p images
 mkdir -p OVMF_files
 
-# Set-up the server
-sudo LD_LIBRARY_PATH=$LD_LIBRARY_PATH ${QEMU_IMG_BIN} convert jammy-server-cloudimg-amd64.img ./images/gdpr-server.img
-sudo LD_LIBRARY_PATH=$LD_LIBRARY_PATH  ${QEMU_IMG_BIN} resize ./images/gdpr-server.img +30G
+# Set-up the controller VM
+sudo LD_LIBRARY_PATH=$LD_LIBRARY_PATH ${QEMU_IMG_BIN} convert jammy-server-cloudimg-amd64.img ./images/controller.img
+sudo LD_LIBRARY_PATH=$LD_LIBRARY_PATH  ${QEMU_IMG_BIN} resize ./images/controller.img +30G
+bash prepare_net_cfg.sh -br virbr0 -cfg ./cloud_configs/network-config-controller.yml
+sudo cloud-localds -N ./cloud_configs/network-config-controller.yml ./images/controller-cloud-config.iso ./cloud_configs/cloud-config-controller
+mkdir -p OVMF_files/controller
+cp ${OVMF_CODE} ./OVMF_files/controller/OVMF_CODE.fd
+cp ${OVMF_VARS} ./OVMF_files/controller/OVMF_VARS.fd
+
+# Set-up the server VM
+sudo LD_LIBRARY_PATH=$LD_LIBRARY_PATH ${QEMU_IMG_BIN} convert jammy-server-cloudimg-amd64.img ./images/server.img
+sudo LD_LIBRARY_PATH=$LD_LIBRARY_PATH  ${QEMU_IMG_BIN} resize ./images/server.img +30G
 bash prepare_net_cfg.sh -br virbr0 -cfg ./cloud_configs/network-config-server.yml
 sudo cloud-localds -N ./cloud_configs/network-config-server.yml ./images/server-cloud-config.iso ./cloud_configs/cloud-config-server
 mkdir -p OVMF_files/server
 cp ${OVMF_CODE} ./OVMF_files/server/OVMF_CODE.fd
 cp ${OVMF_VARS} ./OVMF_files/server/OVMF_VARS.fd
 
-# Set-up the client
-sudo LD_LIBRARY_PATH=$LD_LIBRARY_PATH ${QEMU_IMG_BIN} convert jammy-server-cloudimg-amd64.img ./images/gdpr-client.img
-sudo LD_LIBRARY_PATH=$LD_LIBRARY_PATH  ${QEMU_IMG_BIN} resize ./images/gdpr-client.img +30G
+# Set-up the client VM
+sudo LD_LIBRARY_PATH=$LD_LIBRARY_PATH ${QEMU_IMG_BIN} convert jammy-server-cloudimg-amd64.img ./images/client.img
+sudo LD_LIBRARY_PATH=$LD_LIBRARY_PATH  ${QEMU_IMG_BIN} resize ./images/client.img +30G
 bash prepare_net_cfg.sh -br virbr0 -cfg ./cloud_configs/network-config-client.yml
 sudo cloud-localds -N ./cloud_configs/network-config-client.yml ./images/client-cloud-config.iso ./cloud_configs/cloud-config-client
 mkdir -p OVMF_files/client
