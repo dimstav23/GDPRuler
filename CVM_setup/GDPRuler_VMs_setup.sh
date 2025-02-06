0f366c03a377e035b0e9712079b92a04fe8392f7
dimstav23 <dimstav23@gmail.com>
2025-02-06 14:36:44 +0000

    remove client VM from the configuration as it is not really needed
    Signed-off-by: dimstav23 <dimstav23@gmail.com>

--- CVM_setup/GDPRuler_VMs_setup.sh
+++ CVM_setup/GDPRuler_VMs_setup.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-# Script to setup up a server & client VM for GDPRuler
+# Script to setup up a server and controller VM images for GDPRuler
 
 THIS_DIR=$(dirname "$(readlink -f "$0")")
 
@@ -91,14 +91,14 @@ if [[ ! -f ${THIS_DIR}/${CLOUD_IMG} ]] ; then
 fi
 
 # Set-up the controller VM
-echo "[1/7] Setting up the controller VM files"
+echo "[1/5] Setting up the controller VM files"
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH ${QEMU_IMG_BIN} convert ${THIS_DIR}/${CLOUD_IMG} ${IMAGES_DIR}/controller.img
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH ${QEMU_IMG_BIN} resize ${IMAGES_DIR}/controller.img +20G
 mkdir -p ${OVMF_FILES_DIR}/controller
 cp ${OVMF} ${OVMF_FILES_DIR}/controller/OVMF.fd
 cp ${OVMF_CODE} ${OVMF_FILES_DIR}/controller/OVMF_CODE.fd
 cp ${OVMF_VARS} ${OVMF_FILES_DIR}/controller/OVMF_VARS.fd
-echo "[2/7] Installing software in the controller VM image -- to be used as a base"
+echo "[2/5] Installing software in the controller VM image -- to be used as a base"
 virt-customize --add ${IMAGES_DIR}/controller.img \
   --root-password password:123456 \
   --edit '/etc/ssh/sshd_config:s/#PermitRootLogin prohibit-password/PermitRootLogin yes/' \
@@ -113,35 +113,21 @@ virt-customize --add ${IMAGES_DIR}/controller.img \
   --run-command /root/setup_vm.sh
 
 # Set-up the server VM
-echo "[3/7] Setting up the server VM files"
+echo "[3/5] Setting up the server VM files"
 mkdir -p ${OVMF_FILES_DIR}/server
 cp ${OVMF} ${OVMF_FILES_DIR}/server/OVMF.fd
 cp ${OVMF_CODE} ${OVMF_FILES_DIR}/server/OVMF_CODE.fd
 cp ${OVMF_VARS} ${OVMF_FILES_DIR}/server/OVMF_VARS.fd
 cp ${IMAGES_DIR}/controller.img ${IMAGES_DIR}/server.img
 
-# Set-up the client VM
-echo "[4/7] Setting up the client VM files"
-mkdir -p ${OVMF_FILES_DIR}/client
-cp ${OVMF} ${OVMF_FILES_DIR}/client/OVMF.fd
-cp ${OVMF_CODE} ${OVMF_FILES_DIR}/client/OVMF_CODE.fd
-cp ${OVMF_VARS} ${OVMF_FILES_DIR}/client/OVMF_VARS.fd
-cp ${IMAGES_DIR}/controller.img ${IMAGES_DIR}/client.img
-
 # Import the network config for the controller in the VM image
-echo "[5/7] Setting up the controller network configuration"
+echo "[4/5] Setting up the controller network configuration"
 bash ${THIS_DIR}/prepare_net_cfg.sh -br ${BRIDGE_NAME} -cfg ${THIS_DIR}/network_configs/netplan-controller.yaml
 virt-customize --add ${IMAGES_DIR}/controller.img \
   --copy-in ${THIS_DIR}/network_configs/netplan-controller.yaml:/etc/netplan/
 
 # Import the network config for the server in the VM image
-echo "[6/7] Setting up the server network configuration"
+echo "[5/5] Setting up the server network configuration"
 bash ${THIS_DIR}/prepare_net_cfg.sh -br ${BRIDGE_NAME} -cfg ${THIS_DIR}/network_configs/netplan-server.yaml
 virt-customize --add ${IMAGES_DIR}/server.img \
   --copy-in ${THIS_DIR}/network_configs/netplan-server.yaml:/etc/netplan/
-
-# Import the network config for the client in the VM image
-echo "[7/7] Setting up the client network configuration"
-bash ${THIS_DIR}/prepare_net_cfg.sh -br ${BRIDGE_NAME} -cfg ${THIS_DIR}/network_configs/netplan-client.yaml
-virt-customize --add ${IMAGES_DIR}/client.img \
-  --copy-in ${THIS_DIR}/network_configs/netplan-client.yaml:/etc/netplan/
