8a9725f26624f36e95453575a4645b5f29401f4b
dimstav23 <dimstav23@gmail.com>
2025-08-11 12:50:30 +0000

    update automated runners with the repeats parameter
    Signed-off-by: dimstav23 <dimstav23@gmail.com>

--- evaluation/VM/automated_runner.sh
+++ evaluation/VM/automated_runner.sh
@@ -1,14 +1,28 @@
 #!/bin/sh
 
-# DB server in CVM
-./direct.sh --encryption OFF --logging OFF --server_connection TCP
+repeats=5
 
-# passthrough controller + DB server in CVM
-./passthrough.sh --encryption OFF --logging OFF --server_connection UNIX
-./passthrough.sh --encryption ON --logging OFF --server_connection UNIX
-# ./passthrough.sh --encryption ON --logging ON
+for i in $(seq 1 "$repeats"); do
+  echo "=== Iteration $i / $repeats ==="
 
-# GDPRuler + DB server in CVM
-./gdpr.sh --encryption OFF --logging OFF --server_connection UNIX
-./gdpr.sh --encryption ON --logging OFF --server_connection UNIX
-# ./gdpr.sh --encryption ON --logging ON
+  # -------------------------------
+  # DB server in CVM
+  # -------------------------------
+  ./direct.sh --encryption OFF --logging OFF --server_connection TCP
+
+  # -------------------------------
+  # passthrough controller + DB server in CVM
+  # -------------------------------
+  # ./passthrough.sh --encryption OFF --logging OFF --server_connection UNIX
+  # ./passthrough.sh --encryption ON --logging OFF --server_connection UNIX
+  # ./passthrough.sh --encryption ON --logging ON
+
+  # -------------------------------
+  # GDPRuler + DB server in CVM
+  # -------------------------------
+  ./gdpr.sh --encryption OFF --logging OFF --server_connection UNIX
+  ./gdpr.sh --encryption ON --logging OFF --server_connection UNIX
+  # ./gdpr.sh --encryption ON --logging ON
+
+  echo
+done
--- evaluation/bare_metal/automated_runner.sh
+++ evaluation/bare_metal/automated_runner.sh
@@ -1,20 +1,34 @@
 #!/bin/sh
 
-# direct execution
-./direct.sh --encryption OFF --logging OFF --server_connection TCP
+repeats=5   # how many times to run each test
 
-# native controller execution
-./passthrough.sh --encryption OFF --logging OFF --server_connection UNIX
-./passthrough.sh --encryption OFF --logging OFF --server_connection TCP
-# ./passthrough.sh --encryption OFF --logging ON
-./passthrough.sh --encryption ON --logging OFF --server_connection UNIX
-./passthrough.sh --encryption ON --logging OFF --server_connection TCP
-# ./passthrough.sh --encryption ON --logging ON
+for i in $(seq 1 "$repeats"); do
+  echo "=== Iteration $i / $repeats ==="
 
-# gdpr controller execution
-./gdpr.sh --encryption OFF --logging OFF --server_connection UNIX
-./gdpr.sh --encryption OFF --logging OFF --server_connection TCP
-# ./gdpr.sh --encryption OFF --logging ON
-./gdpr.sh --encryption ON --logging OFF --server_connection UNIX
-./gdpr.sh --encryption ON --logging OFF --server_connection TCP
-# ./gdpr.sh --encryption ON --logging ON
+  # -------------------------------
+  # direct execution
+  # -------------------------------
+  ./direct.sh --encryption OFF --logging OFF --server_connection TCP
+
+  # -------------------------------
+  # passthrough controller execution
+  # -------------------------------
+  # ./passthrough.sh --encryption OFF --logging OFF --server_connection UNIX
+  # ./passthrough.sh --encryption OFF --logging OFF --server_connection TCP
+  # ./passthrough.sh --encryption OFF --logging ON
+  # ./passthrough.sh --encryption ON  --logging OFF --server_connection UNIX
+  # ./passthrough.sh --encryption ON  --logging OFF --server_connection TCP
+  # ./passthrough.sh --encryption ON  --logging ON
+
+  # -------------------------------
+  # GDPR controller execution
+  # -------------------------------
+  ./gdpr.sh --encryption OFF --logging OFF --server_connection UNIX
+  # ./gdpr.sh --encryption OFF --logging OFF --server_connection TCP
+  # ./gdpr.sh --encryption OFF --logging ON
+  ./gdpr.sh --encryption ON  --logging OFF --server_connection UNIX
+  # ./gdpr.sh --encryption ON  --logging OFF --server_connection TCP
+  # ./gdpr.sh --encryption ON  --logging ON
+
+  echo
+done
